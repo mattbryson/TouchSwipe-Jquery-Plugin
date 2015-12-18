6018a7fac5a86761eab5cbc33f10b31ffb403606
Matt Bryson <mattbryson@gmail.com>
2015-12-18 15:28:32 +0000

    #223 added common JS to factory" git status

--- jquery.touchSwipe.js
+++ jquery.touchSwipe.js
@@ -142,6 +142,9 @@
     if (typeof define === 'function' && define.amd && define.amd.jQuery) {
         // AMD. Register as anonymous module.
         define(['jquery'], factory);
+    } else if (typeof module !== 'undefined' && module.exports) {
+        // CommonJS Module
+        factory(require("jquery"));
     } else {
         // Browser globals.
         factory(jQuery);
